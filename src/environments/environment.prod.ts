696dc9d05dce6db6d909b6e3d8c0ac665400da41
larsbeckmusic <larsbeckmusic@gmail.com>
2026-04-24 11:52:34 +0200

    added supabase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://YOUR-PROJECT.supabase.co',
+    anonKey: 'YOUR-ANON-KEY',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://YOUR-PROJECT.supabase.co',
+    anonKey: 'YOUR-ANON-KEY',
+  },
+};
