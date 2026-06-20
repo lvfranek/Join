841fc026890b21f9cd330cf321b35d6581a74d7e
Franek Kaminski <xeven770@gmail.com>
2026-06-20 17:05:57 +0200

    github fork fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   provider: 'supabase' as 'supabase' | 'mariadb',
   apiUrl: 'http://localhost:3000/api',
   supabase: {
-    url: 'https://njtneoycnavxqbhnejcf.supabase.co',
-    anonKey: 'sb_publishable_TGaYmc58dDi9yDJaL27qCw_HmW4Zcwi',
+    url: 'https://YOUR-PROJECT.supabase.co',
+    anonKey: 'YOUR-ANON-KEY',
   },
 };
